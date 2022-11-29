517a9187a421722ffa7a4cedf994ef2e1f9201b1
Guilherme Loeblein <89669208+guuiloeblein@users.noreply.github.com>
2022-11-29 10:29:26 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -23,14 +23,17 @@ const jump = () => {
         const pipePosition = pipe.offsetLeft;
         const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
+        console.log(pipePosition);
+        console.log(marioPosition);
+
         
         if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
 
-            pipe.style.animation = 'none';
-            pipe.style.left = `${pipePosition}px`;
-
             mario.style.animation = 'none';
             mario.style.bottom = `${marioPosition}px`;
+
+            pipe.style.animation = 'none';
+            pipe.style.left = `${pipePosition}px`;
 
             mario.src = 'assets/image/game-over.png';
             mario.style.width = '65px';
@@ -43,6 +46,7 @@ const jump = () => {
 
     }, 10);
 }
+
 
 document.addEventListener('keydown', jump); 
 
